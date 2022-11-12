d62716894ca503bb335b6d03c454f9f927c80387
s173832 <s173832@student.dtu.dk>
2022-11-12 18:19:21 +0100

    I have added a category matching system, which also sort out the EPDs that do not fit into the categories. It seems to work, but there are still some issues with the functional unit

--- Level2_function.py
+++ Level2_function.py
@@ -1,16 +1,9 @@
 from functions import *
+from typing import List
+import json
 
-def get_epd_info_level_2(epd_data: dict, classification_list: list, name_list: list, GWP_list: list, IBU_categories_list: list, uuid_list: list):
-    #print('hej')
-    if 'modellingAndValidation' in epd_data and 'LCIMethodAndAllocation' in epd_data['modellingAndValidation'] and \
-            'referenceToLCAMethodDetails' in epd_data['modellingAndValidation']['LCIMethodAndAllocation'] and \
-            'shortDescription' in epd_data['modellingAndValidation']['LCIMethodAndAllocation']['referenceToLCAMethodDetails'][0] and \
-            'value' in epd_data['modellingAndValidation']['LCIMethodAndAllocation']['referenceToLCAMethodDetails'][0]['shortDescription'][0]:
-        classification_value = epd_data['modellingAndValidation']['LCIMethodAndAllocation']['referenceToLCAMethodDetails'][0]['shortDescription'][0]['value']
-        classification_list.append(classification_value)
-        #print(classification_value)
-    else:
-        classification_list.append('No value')
+
+def get_epd_info_level_2(epd_data: dict, name_list: list, gwp_list: list, uuid_list: list):
 
     if 'value' in epd_data['processInformation']['dataSetInformation']['name']['baseName'][0]:
 
@@ -19,8 +12,6 @@ def get_epd_info_level_2(epd_data: dict, classification_list: list, name_list: l
     else:
         name_list.append('No value')
 
-
-# TODO: find det sted i 'LCIAResult', der indeholder 'Global warming potential (GWP)'
     temp = []
     if 'anies' in epd_data['LCIAResults']['LCIAResult'][findgwp(epd_data['LCIAResults']['LCIAResult'])]['other']:
 
@@ -32,31 +23,98 @@ def get_epd_info_level_2(epd_data: dict, classification_list: list, name_list: l
             temp.append('No value')
     else:
         temp.append('No value')
-    GWP_list.append(temp)
-
-    tempp = []
-    if 'classification' in epd_data['processInformation']['dataSetInformation']['classificationInformation']:
-        IBUindex = findIBUcategories(epd_data['processInformation']['dataSetInformation']['classificationInformation']['classification'])
-
-        class_list = epd_data['processInformation']['dataSetInformation']['classificationInformation']['classification'][IBUindex]['class']
-        for element in class_list:
-            if 'Bauprodukte' in element['value']:
-                continue
-            tempp.append(element['value'])
-
-    else:
-        tempp.append('No value')
-
-    IBU_categories_list.append(tempp)
+    gwp_list.append(temp)
 
     if 'technologyDescriptionAndIncludedProcesses' in epd_data['processInformation']['technology']:
         add_functional_unit(epd_data['processInformation']['technology']['technologyDescriptionAndIncludedProcesses'])
 
-
-
-# TODO: find 'UUID'
+    # TODO: find 'UUID'
     if 'UUID' in epd_data['processInformation']['dataSetInformation']:
         uuid_value = epd_data['processInformation']['dataSetInformation']['UUID']
         uuid_list.append(uuid_value)
     else:
         uuid_list.append('No value')
+
+
+def find_ibu_classification(epd_data_json: dict):
+    """
+    This function finds the IBU classification for the EPD. This classification is used to sort the EPDs into
+    categories in the function match_categories. Or it registrers if there is
+    :param epd_data_json: The data for one EPD in json format
+    :return: if there are classifications a list of strings is returned, otherwise the string saying 'No value'
+    """
+    classification_list = []
+
+    if 'classification' in epd_data_json['processInformation']['dataSetInformation']['classificationInformation']:
+        IBUindex = findIBUcategories(epd_data_json['processInformation']['dataSetInformation']['classificationInformation']['classification'])
+
+        class_list = epd_data_json['processInformation']['dataSetInformation']['classificationInformation']['classification'][IBUindex]['class']
+        for element in class_list:
+            if 'Bauprodukte' in element['value']:
+                continue
+            classification_list.append(element['value'])
+
+    else:
+        classification_list.append('No value')
+
+    return classification_list
+
+
+def match_categories(sorted_category_path: str, epd_ibu_categories: List[str]):
+    """
+    This function matches the IBU classification to a manually created category system, which sorts the EPDs into
+    building components (level 1) and materials (level 2). Some epds fit into two categories, which is indicated with
+    a list of categories in the category data structure. In such a situation the EPD will be duplicated and put into
+    both categories.
+    :param sorted_category_path: str: path name of the json file with the category data structure. 'categories.json'
+    :param epd_ibu_categories: The ibu classification categories found with find_ibu_classification
+    :return:    'No match' if there is no match
+                If matching two categories, two lists with the category names, two lists with lifespan and a True
+                boolean for duplicating the epd
+                If matching one category, one list with category names, one list with lifespan and a False boolean
+    """
+    # The json with sorted categories to match with is read from file
+    with open(sorted_category_path, 'r', encoding='utf8') as f:
+        sorted_categories = json.load(f)
+
+    # matching the first classification level
+    if epd_ibu_categories[0] in sorted_categories:
+
+        # Matching the second classification level
+        if epd_ibu_categories[1] in sorted_categories[epd_ibu_categories[0]]['pcr']:
+
+            # Checking if the EPD fit in multiple categories
+            if isinstance(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_1_category'], List):
+                temp_level_category_list = []
+                temp_level_category_list_2 = []
+
+                # Appending the different level 1 categories
+                temp_level_category_list.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_1_category'][0])
+                temp_level_category_list_2.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_1_category'][1])
+
+                # Appending two level 2 categories
+                temp_level_category_list.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_2_category'])
+                temp_level_category_list_2.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_2_category'])
+
+                # Appending two lifetimes
+                temp_lifespan = sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['lifetime']
+                temp_lifespan_2 = sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['lifetime']
+
+                # This EPD will need to be duplicated
+                duplicated = True
+
+                return temp_level_category_list, temp_level_category_list_2, temp_lifespan, temp_lifespan_2, duplicated
+
+            # If the EPD only fints into one category
+            elif isinstance(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_1_category'], str):
+                temp_level_category_list = []
+                temp_level_category_list.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_1_category'])
+                temp_level_category_list.append(sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['level_2_category'])
+                temp_lifespan = sorted_categories[epd_ibu_categories[0]]['pcr'][epd_ibu_categories[1]]['lifetime']
+                duplicated = False
+
+                return temp_level_category_list, temp_lifespan, duplicated
+        else:
+            return 'No match'
+    else:
+        return 'No match'
--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from requests.auth import HTTPBasicAuth
 import json
 from functions import add_phases_to_set
 from Level1_function import get_epd_info_level_1
-from Level2_function import get_epd_info_level_2
-
+from Level2_function import get_epd_info_level_2, find_ibu_classification, match_categories
 from typing import List
 
 # Dictionary to gather information
 overview = {}
 
-
+# Lists to gather information
 set_of_phases = set()
 name_list = []  # One list with all the names
 functional_unit_list = []  # One list with all the functional units
-classification_list = []
-GWP_list = []# A list with lists that has this order of stages
-IBU_categories_list = []
+category_list = []
+GWP_list = []  # A list with lists that has this order of stages
+lifespan_list = []
 uuid_list = []
 
 #                                                              __STEP 1__
@@ -24,35 +23,67 @@ uuid_list = []
 URL = 'https://data.eco-platform.org/resource/processes?search=true&distributed=true&virtual=true&metaDataOnly=false'
 
 # The token and username gotten from the ECO portal
-#authorization = HTTPBasicAuth('laerkeV', '')
 token = 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJsYWVya2VWIiwiaXNzIjoiRUNPUE9SVEFMIiwiYXVkIjoiYW55IiwidmVyIjoiNy40LjIiLCJwZXJtaXNzaW9ucyI6WyJzdG9jazpyZWFkLGV4cG9ydDoyIiwic3RvY2s6cmVhZCxleHBvcnQ6MSIsInVzZXI6cmVhZCx3cml0ZToxNTQiXSwicm9sZXMiOltdLCJpYXQiOjE2NjMxNjQ5ODIsImV4cCI6MTY3MTA0ODk4MiwiZW1haWwiOiJzMTczODMyQHN0dWRlbnQuZHR1LmRrIiwidGl0bGUiOiIiLCJmaXJzdE5hbWUiOiJMYWVya2UiLCJsYXN0TmFtZSI6IlZlanNuYWVzIiwiZ2VuZXJhdGVOZXdUb2tlbnMiOmZhbHNlLCJqb2JQb3NpdGlvbiI6IlN0dWRlbnQiLCJhZGRyZXNzIjp7ImNpdHkiOiJLb25nZW5zIEx5bmdieSIsInppcENvZGUiOiIyODAwIiwiY291bnRyeSI6IkRLIiwic3RyZWV0IjoiIn0sIm9yZ2FuaXphdGlvbiI6e30sInVzZXJHcm91cHMiOlt7InVzZXJHcm91cE5hbWUiOiJyZWdpc3RlcmVkX3VzZXJzIiwidXNlckdyb3VwT3JnYW5pemF0aW9uTmFtZSI6IkRlZmF1bHQgT3JnYW5pemF0aW9uIn1dLCJhZG1pbmlzdHJhdGVkT3JnYW5pemF0aW9uc05hbWVzIjoiIiwicGhvbmUiOiIiLCJkc3B1cnBvc2UiOiJGb3IgYSBzY2hvb2wgcHJvamVjdCBhdCBteSBtYXN0ZXJzIGluIEFyY2hpdGVjdHVyYWwgRW5nZW5lZXJpbmcgYXQgRFRVLiBXZSBhcmUgYSBncm91cCBvZiBzdHVkZW50cyB0aGF0IGFyZSB3b2tpbmcgb24gbGVhbmluZyBob3cgdG8gd29yayB3aXRoIExDQSBhbmQgRVBEcy4iLCJzZWN0b3IiOiIiLCJpbnN0aXR1dGlvbiI6IlRlY2huaWNhbCBVbml2ZXJzaXR5IG9mIERlbm1hcmsgLSBEVFUifQ.VWCEmxgB4HSDCYJ7tdZtx-ZZWzxNVgLDXKH4-PqZEzgbBl2erK63MLtGigartJ0no6LDZuThbVxNzD10-IMjrcIn_bKEmo60gnFc9xXTPGZVjL4CybRPhdPx1lEfGFD_zh8r3mhhuUJrf73vtdVVnK-DQ0e4ji7-W2dl0NnuYVI'
 
-# The parameters that we need when we send request for each epd
-parameters = '&format=json&view=extended'
-
-# Parameters to sech for
-meta_parameters = {'pageSize': '300', 'location': 'DE', 'validUntil': '2022', 'format': 'JSON', 'classes': 'bauprodukte'}
+# Parameters to search for
+meta_parameters = {'pageSize': '3000', 'validUntil': '2022', 'format': 'JSON', 'classes': 'bauprodukte'}
 
 # The initial request is made and turned into json, so that we can get the information about each epd
 initial_request = requests.get(url=URL, headers={'Authorization': token}, params=meta_parameters)
 data = initial_request.json()
 
 #                                                               __STEP 2__
+
+# The parameters that we need when we send request for each epd
+parameters = '&format=json&view=extended'
+
 # Loop going through each of the epds that we have requested. Skips EPD if something goes wrong.
 counter = 0
 for epd in data['data']:
+
+    # Todo: delete this when handing in, it is for the work process, not having to run all epds every time
     if counter > 10:
         print("stop")
 
     epd_url = epd['uri']  # This is the url that we need to make a request
 
-    # TODO: here we could sort the epds from the initial info. Some are not relevant and we can already see that here.
     search_url = f'{epd_url}{parameters}'  # We are concatenating our url with the parameters that we need.
 
     epd_data = requests.get(search_url, headers={'Authorization': token})
     if epd_data.status_code == 200:
         epd_data_json = epd_data.json()  # This is the json with the information that we need to get all the information
-        get_epd_info_level_2(epd_data_json, classification_list, name_list, GWP_list, IBU_categories_list, uuid_list)
+
+        epd_ibu_classification = find_ibu_classification(epd_data_json)
+
+        # If the EPD does not have any ibu classification, we do not use it
+        if epd_ibu_classification == 'No value':
+            break
+
+        # matching the ibu classification to our own categories
+        match = match_categories('categories.json', epd_ibu_classification)
+
+        # We only use the EPD if there is a match
+        if match != 'No match':
+
+            # If the EPD fits into multiple categories it will be duplicated
+            if match[-1]:
+                categories_1, categories_2, lifespan_1, lifespan_2 = match[0], match[1], match[2], match[3]
+
+                category_list.append(categories_1)
+                lifespan_list.append(lifespan_1)
+                get_epd_info_level_2(epd_data_json, name_list, GWP_list, uuid_list)
+
+                category_list.append(categories_2)
+                lifespan_list.append(lifespan_2)
+                get_epd_info_level_2(epd_data_json, name_list, GWP_list, uuid_list)
+
+            # If the EPD just fits into one category is will only apper once in the final data structure
+            else:
+                categories_1, lifespan_1 = match[0], match[1]
+
+                category_list.append(categories_1)
+                lifespan_list.append(lifespan_1)
+                get_epd_info_level_2(epd_data_json, name_list, GWP_list, uuid_list)
 
         print(counter)
 
@@ -73,7 +104,8 @@ print(overview)
 overview['name_list'] = name_list
 overview['functional_unit_list'] = functional_unit_list
 overview['GWP_list'] = GWP_list
-overview['IBU_categories_list'] = IBU_categories_list
+overview['category_list'] = category_list
+overview['lifespan_list'] = lifespan_list
 
 data_clean = json.dumps(overview, indent=4)
 with open('json_epd_classific_overblik.json', 'w', encoding='utf8') as f:
